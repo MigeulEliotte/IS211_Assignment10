ade323691e1d2687ba82148550119fd7923ae0a0
MigeulEliotte <97182631+MigeulEliotte@users.noreply.github.com>
2024-11-06 21:21:07 -0500

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,27 @@
 
+--Yeats Collection
+
+-- Create the artists table
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+-- Create the albums table
+CREATE TABLE albums (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    artist_id INTEGER NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
+
+-- Create the songs table
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    album_id INTEGER NOT NULL,
+    track_number INTEGER NOT NULL,
+    duration_seconds INTEGER NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albums(id)
+);
+
